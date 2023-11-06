a077c13c20a9f3cc16125a4753ba73e8d42a9ab2
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2023-11-06 10:22:16 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = (my_string.indexOf(is_prefix) == 0)? 1 : 0;
+        
+        return answer;
+    }
+}
