5d5b9c96a272cbb4d71168f7eca01fcbc0f30f2e
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2023-11-06 10:39:19 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(int i = 0; i < my_string.length(); i++){
+            int ch = my_string.charAt(i);
+            
+            if(ch >= 65 && ch <= 90){
+                answer[ch-65]++;
+            }
+            else {
+                answer[ch-97+26]++;
+            }
+        }
+        
+        return answer;
+    }
+}
