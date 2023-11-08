095ace8082730eb2d91b8f2a801c39576e270e5b
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2023-11-08 16:02:10 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 7.52 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = (myString.toUpperCase().indexOf(pat.toUpperCase()) > -1)? 1 : 0;
+        
+        return answer;
+    }
+}
