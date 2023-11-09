5c6a07753ebe2e3aa8352d137506b656deab4146
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2023-11-09 18:14:37 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String binomial) {
+        int answer = 0;
+        String op = binomial.split(" ")[1];
+        int a = Integer.parseInt(binomial.split(" ")[0]);
+        int b = Integer.parseInt(binomial.split(" ")[2]);
+        
+        switch(op){
+            case "+" : answer = a + b; break;
+            case "-" : answer = a - b; break;
+            case "*" : answer = a * b; break;
+        }
+        
+        return answer;
+    }
+}
