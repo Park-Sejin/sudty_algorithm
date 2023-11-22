c2b0f6daeed8bc9ac83905608ca2cb44b241edcc
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2023-11-22 14:33:08 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = (n % 2 == 0)? n/2 : n/2+1;
+        int[] answer = new int[size];
+        
+        int j = 0;
+        for(int i = 0; i <= n; i++){
+            if(i % 2 != 0){
+                answer[j++] = i;
+            }
+        }
+        return answer;
+    }
+}
