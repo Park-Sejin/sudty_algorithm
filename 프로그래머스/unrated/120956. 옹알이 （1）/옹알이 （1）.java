b4cfadb8b979273c70c093128f251389c63235a5
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2023-11-28 15:39:08 +0900

    [level unrated] Title: 옹알이 (1), Time: 1.48 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        for(String str : babbling){
+            str = str.replaceAll("(aya)*(ye)*(woo)*(ma)*", "");
+            
+            if(str.length() == 0){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
