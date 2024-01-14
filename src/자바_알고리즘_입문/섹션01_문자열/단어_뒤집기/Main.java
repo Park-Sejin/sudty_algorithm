ab419ccf9fdca3be66d595a73adc8c529077a2f3
Park-Sejin <stsj84@gmail.com>
2024-01-14 23:39:18 +0900

    [2024/01/14] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. String(문자열)  - 단어 뒤집기 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션01_문자열/단어_뒤집기/Main.java
+++ src/자바_알고리즘_입문/섹션01_문자열/단어_뒤집기/Main.java
@@ -5,6 +5,30 @@ import java.util.*;
 class Main {
 	public void solution(String[] str_arr){
 		for(String str : str_arr) {
+			/*
+			 * 풀이 2
+			 */
+			char[] ch_arr = str.toCharArray();
+			int lt = 0, rt = str.length()-1;
+			
+			while(lt < rt) {
+				char temp = ch_arr[lt];
+				
+				ch_arr[lt++] = ch_arr[rt];
+				ch_arr[rt--] = temp;
+			}
+			
+			System.out.println(String.valueOf(ch_arr));
+			
+			/*
+			 * 풀이 1
+			String answer = new StringBuilder(str).reverse().toString();
+			
+			System.out.println(answer);
+			*/
+			
+			/*
+			 * 내가 제출한 코드
 			String answer = "";
 			
 			for(int i = str.length()-1; i >= 0 ; i--) {
@@ -12,6 +36,7 @@ class Main {
 			}
 			
 			System.out.println(answer);
+			*/
 		}
 	}
 	
