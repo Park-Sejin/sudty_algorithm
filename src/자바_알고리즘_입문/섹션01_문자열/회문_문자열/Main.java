ab7b22a80e6af0c43303f06cd8ae4d42c6785e2b
Park-Sejin <stsj84@gmail.com>
2024-01-21 22:33:18 +0900

    [2024/01/21] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. String(문자열)  - 회문 문자열

---
+++ src/자바_알고리즘_입문/섹션01_문자열/회문_문자열/Main.java
@@ -0,0 +1,30 @@
+package 자바_알고리즘_입문.섹션01_문자열.회문_문자열;
+
+import java.util.*;
+
+class Main {
+	public String solution(String str){
+		String answer = "YES";
+		int lt = 0, rt = str.length()-1;
+		
+		while(lt < rt) {
+			int lt_ch = Character.toUpperCase(str.charAt(lt++));
+			int rt_ch = Character.toUpperCase(str.charAt(rt--));
+			
+			if(lt_ch != rt_ch) {
+				return "NO";
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner in = new Scanner(System.in);
+		
+		String input1 = in.next();
+		
+		System.out.println(T.solution(input1));
+	}
+}
