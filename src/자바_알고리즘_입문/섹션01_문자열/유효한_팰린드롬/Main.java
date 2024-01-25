8e7b76210595649fca3d14eb071a71b859f03534
Park-Sejin <stsj84@gmail.com>
2024-01-25 23:08:52 +0900

    [2024/01/25] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. String(문자열)  - 유효한 팰린드롬

---
+++ src/자바_알고리즘_입문/섹션01_문자열/유효한_팰린드롬/Main.java
@@ -0,0 +1,42 @@
+package 자바_알고리즘_입문.섹션01_문자열.유효한_팰린드롬;
+
+import java.util.*;
+
+class Main {
+	public String solution(String str){
+		String answer = "YES";
+		int lt = 0, rt = str.length()-1;
+		
+		while(lt < rt) {
+			char lt_ch = Character.toUpperCase(str.charAt(lt));
+			char rt_ch = Character.toUpperCase(str.charAt(rt));
+			
+			if(!Character.isAlphabetic(lt_ch)) {
+				lt++;
+			}
+			else if(!Character.isAlphabetic(rt_ch)) {
+				rt--;
+			}
+			else {
+				if(lt_ch != rt_ch) {
+					return "NO";
+				}
+				
+				lt++;
+				rt--;
+			}
+		}
+		
+		
+		return answer;
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner in = new Scanner(System.in);
+		
+		String input1 = in.nextLine();
+		
+		System.out.println(T.solution(input1));
+	}
+}
