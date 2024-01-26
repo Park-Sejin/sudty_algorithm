63299a3e1cedb7838620a350341b12c5f11a0f0e
Park-Sejin <stsj84@gmail.com>
2024-01-26 23:54:26 +0900

    [2024/01/26] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. String(문자열)  - 숫자만 추출 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션01_문자열/숫자만_추출/Main.java
+++ src/자바_알고리즘_입문/섹션01_문자열/숫자만_추출/Main.java
@@ -3,10 +3,35 @@ package 자바_알고리즘_입문.섹션01_문자열.숫자만_추출;
 import java.util.*;
 
 class Main {
-	public String solution(String str){
-		String answer = str.replaceAll("[^0-9]","").replaceAll("^0+", "");
+	public int solution(String str){
+		String answer = "";
+		
+		for(char ch : str.toCharArray()) {
+			// 숫자인지 판별
+			if(Character.isDigit(ch)) {
+				answer += ch;
+			}
+		}
+		
+		return Integer.parseInt(answer);
+		
+		/*
+		int answer = 0;
+		
+		for(char ch : str.toCharArray()) {
+			if(ch >= '0' && ch <= '9') {
+				answer = answer * 10 + (ch-'0');
+			}
+		}
 		
 		return answer;
+		*/
+		
+		/*
+		int answer = Integer.parseInt(str.replaceAll("[^0-9]",""));
+		
+		return answer;
+		*/
 	}
 	
 	public static void main(String[] args){
