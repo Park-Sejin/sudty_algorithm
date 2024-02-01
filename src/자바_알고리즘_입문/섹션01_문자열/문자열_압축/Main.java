f46ee3dc8ffb0a1a5da94d1068044950ae8df7b9
Park-Sejin <stsj84@gmail.com>
2024-02-01 20:17:11 +0900

    [2024/02/01] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. String(문자열)  - 문자열 압축 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션01_문자열/문자열_압축/Main.java
+++ src/자바_알고리즘_입문/섹션01_문자열/문자열_압축/Main.java
@@ -5,6 +5,25 @@ import java.util.*;
 class Main {
 	public String solution(String str){
 		String answer = "";
+		str = str + " ";
+		int cnt = 1;
+		
+		for(int i = 0; i < str.length()-1; i++) {
+			if(str.charAt(i) == str.charAt(i+1)) {
+				cnt++;
+			}
+			else {
+				answer += str.charAt(i);
+				
+				if(cnt > 1) {
+					answer += String.valueOf(cnt);
+					cnt = 1;
+				}
+			}
+		}
+		
+		
+		/*
 		char ch = str.charAt(0);
 		int cnt = 0;
 		
@@ -21,6 +40,7 @@ class Main {
 		}
 		
 		answer += str.charAt(str.length()-1) + ((cnt>1)? cnt+"" : "");
+		*/
 		
 		return answer;
 	}
