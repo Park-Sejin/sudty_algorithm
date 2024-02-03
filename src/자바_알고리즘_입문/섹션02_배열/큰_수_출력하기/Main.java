5b1b519336b28448ae7fc5f07cdbe2f9ee11cda0
Park-Sejin <stsj84@gmail.com>
2024-02-04 00:24:06 +0900

    [2024/02/04] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Array(1, 2차원 배열)  - 큰 수 출력하기 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션02_배열/큰_수_출력하기/Main.java
+++ src/자바_알고리즘_입문/섹션02_배열/큰_수_출력하기/Main.java
@@ -3,7 +3,21 @@ package 자바_알고리즘_입문.섹션02_배열.큰_수_출력하기;
 import java.util.*;
 
 class Main {
-	public String solution(int num, String nums){
+	public void solution(int num, int[] nums){
+		List<Integer> answer = new ArrayList<Integer>();
+		answer.add(nums[0]);
+		
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i-1] < nums[i]) {
+				answer.add(nums[i]);
+			}
+		}
+		
+		for(int n : answer) {
+			System.out.print(n + " ");
+		}
+		
+		/*
 		String[] numbers = nums.split(" ");
 		String answer = numbers[0];
 		
@@ -17,15 +31,25 @@ class Main {
 		}
 		
 		return answer;
+		*/
 	}
 	
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner in = new Scanner(System.in);
 		
+		/*
 		int input1 = Integer.parseInt(in.nextLine());
 		String input2 = in.nextLine();
+		*/
 		
-		System.out.println(T.solution(input1, input2));
+		int input1 = in.nextInt();
+		int[] arr = new int[input1];
+		
+		for(int i = 0; i < input1; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		T.solution(input1, arr);
 	}
 }
