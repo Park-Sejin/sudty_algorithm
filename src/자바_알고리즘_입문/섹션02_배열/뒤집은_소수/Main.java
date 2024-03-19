20a329bf639a0b8ab3bf03616a40bc0d56102e8d
Park-Sejin <stsj84@gmail.com>
2024-03-19 13:28:42 +0900

    [2024/03/19] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Array(1, 2차원 배열)  - 뒤집은 소수 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션02_배열/뒤집은_소수/Main.java
+++ src/자바_알고리즘_입문/섹션02_배열/뒤집은_소수/Main.java
@@ -3,9 +3,37 @@ package 자바_알고리즘_입문.섹션02_배열.뒤집은_소수;
 import java.util.*;
 
 class Main {
-	public void solution(String[] arr){
-		for(String str : arr) {
-			StringBuffer sb = new StringBuffer(str).reverse();
+	public Boolean isPrime(int num) {
+		if(num == 1) {
+			return false;
+		}
+		
+		for(int i = 2; i < num; i++) {
+			if(num%i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public void solution(int[] arr){
+		for(int i = 0; i < arr.length; i++) {
+			int tmp = arr[i];
+			int result = 0;
+			
+			while(tmp > 0) {
+				result = result*10 + tmp%10;
+				tmp = tmp/10;
+			}
+			
+			if(isPrime(result)){
+				System.out.print(result + " ");
+			}
+		}
+		
+		/*for(int n : arr) {
+			StringBuffer sb = new StringBuffer(n+"").reverse();
 			int num = Integer.parseInt(sb.toString());
 			int[] nums = new int[num+1];
 			
@@ -20,7 +48,7 @@ class Main {
 			if(num != 1 && nums[num] == 0) {
 				System.out.print(num + " ");
 			}
-		}
+		}*/
 	}
 	
 	public static void main(String[] args){
@@ -28,10 +56,10 @@ class Main {
 		Scanner in = new Scanner(System.in);
 		
 		int input1 = in.nextInt();
-		String[] arr = new String[input1];
+		int[] arr = new int[input1];
 		
 		for(int i = 0; i < arr.length; i++) {
-			arr[i] = in.next();
+			arr[i] = in.nextInt();
 		}
 		
 		T.solution(arr);
