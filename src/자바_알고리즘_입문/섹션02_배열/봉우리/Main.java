d4bb60d2e5c2f9beaee9f0e10ca41fed4c586173
Park-Sejin <stsj84@gmail.com>
2024-03-20 17:03:25 +0900

    [2024/03/20] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Array(1, 2차원 배열)  - 봉우리 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션02_배열/봉우리/Main.java
+++ src/자바_알고리즘_입문/섹션02_배열/봉우리/Main.java
@@ -3,6 +3,54 @@ package 자바_알고리즘_입문.섹션02_배열.봉우리;
 import java.util.*;
 
 class Main {
+	int[] dx = {-1, 0, 1, 0};
+	int[] dy = {0, 1, 0, -1};
+	
+	public void solution(int n, int[][] arr){
+		int answer = 0;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				boolean flag = true;
+				
+				for(int k = 0; k < 4; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					
+					if(nx >= 0 && nx < n && ny >= 0 && ny < n 
+							&& arr[i][j] <= arr[nx][ny]) {
+						flag = false;
+						break;
+					}
+				}
+				
+				if(flag) {
+					answer++;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		int[][] arr = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = in.nextInt();
+			}
+		}
+		
+		T.solution(n, arr);
+	}
+	
+	
+	/*
 	public void solution(int len, int[][] arr){
 		int answer = 0;
 		
@@ -38,4 +86,5 @@ class Main {
 		
 		T.solution(n+2, arr);
 	}
+	*/
 }
