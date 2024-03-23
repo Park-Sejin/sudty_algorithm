e37f220a60f994d08044a4f4da720f4b5207712e
Park-Sejin <stsj84@gmail.com>
2024-03-24 03:39:30 +0900

    [2024/03/24] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Array(1, 2차원 배열)  - 임시반장 정하기 (인프런 풀이)

--- src/자바_알고리즘_입문/섹션02_배열/임시반장_정하기/Main.java
+++ src/자바_알고리즘_입문/섹션02_배열/임시반장_정하기/Main.java
@@ -8,14 +8,29 @@ class Main {
 		int maxCnt = 0;
 		
 		for(int i = 0; i < n; i++) {
+			int cnt = 0;
+			
+			for(int j = 0; j < n; j++) {
+				for(int k = 0; k < 5; k++) {
+					if(arr[i][k] == arr[j][k]) {
+						cnt++;
+						break;
+					}
+				}
+			}
+			
+			if(cnt > maxCnt) {
+				maxCnt = cnt;
+				answer = i;
+			}
+		}
+		
+		/*
+		for(int i = 0; i < n; i++) {
 			Set<Integer> list = new HashSet<>();
 			
 			for(int j = 0; j < 5; j++) {
 				for(int k = 0; k < n; k++) {
-					if(i == k) {
-						continue;
-					}
-					
 					if(arr[i][j] == arr[k][j]) {
 						list.add(k);
 					}
@@ -27,6 +42,7 @@ class Main {
 				answer = i;
 			}
 		}
+		*/
 		
 		System.out.println(answer+1);
 	}
