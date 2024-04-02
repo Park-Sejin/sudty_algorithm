fbffca0ae77aeaca8f482c36b18aa95b7a93c096
Park-Sejin <stsj84@gmail.com>
2024-04-02 22:46:28 +0900

    2024/04/02] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Array(1, 2차원 배열)  - 멘토링

--- src/자바_알고리즘_입문/섹션02_배열/멘토링/Main.java
+++ src/자바_알고리즘_입문/섹션02_배열/멘토링/Main.java
@@ -3,14 +3,37 @@ package 자바_알고리즘_입문.섹션02_배열.멘토링;
 import java.util.*;
 
 class Main {
-	public void solution(int[][] arr){
+	public void solution(int n, int m, int[][] arr){
 		int answer = 0;
 		
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + " ");
+		for(int i = 0; i < n; i++) {
+			int mto = arr[0][i];
+			
+			for(int j = i+1; j < n; j++) {
+				int mti = arr[0][j];
+				boolean mtoAt = true;
+				
+				for(int k = 1; k < m; k++) {
+					for(int l = 0; l < n; l++) {
+						if(arr[k][l] == mto) {
+							break;
+						}
+						else if(arr[k][l] == mti) {
+							mtoAt = false;
+							break;
+						}
+					}
+					
+					if(!mtoAt) {
+						break;
+					}
+				}
+				
+				if(mtoAt) {
+					answer++;
+				}
 			}
-			System.out.println();
+			
 		}
 		
 		System.out.println(answer);
@@ -20,16 +43,16 @@ class Main {
 		Main T = new Main();
 		Scanner in = new Scanner(System.in);
 		
-		int n1 = in.nextInt();
-		int n2 = in.nextInt();
-		int[][] arr = new int[n2][n1];
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int[][] arr = new int[m][n];
 		
-		for(int i = 0; i < n2; i++) {
-			for(int j = 0; j < n1; j++) {
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
 				arr[i][j] = in.nextInt();
 			}
 		}
 		
-		T.solution(arr);
+		T.solution(n, m, arr);
 	}
 }
