b2b9963ffa5a29a43527485f25ee671ee2300beb
Park-Sejin <stsj84@gmail.com>
2024-04-11 13:24:37 +0900

    [2024/04/11] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Two pointers, Sliding window[효율성 : O(n^2)-->O(n)]  - 연속된 자연수의 합 (+인프런 풀이)

--- src/자바_알고리즘_입문/섹션03_효율성/연속된_자연수의_합/Main.java
+++ src/자바_알고리즘_입문/섹션03_효율성/연속된_자연수의_합/Main.java
@@ -6,8 +6,9 @@ class Main {
 	public void solution(int n){
 		int answer = 0;
 		int sum = 0, lt = 1;
+		int m = n/2+1; // 연속 된 2개의 수이기 때문에 n까지 검사 하지 않아도 됨
 		
-		for(int rt = 1; rt < n; rt++) {
+		for(int rt = 1; rt <= m; rt++) {
 			sum += rt;
 			
 			if(sum == n) { answer++; }
