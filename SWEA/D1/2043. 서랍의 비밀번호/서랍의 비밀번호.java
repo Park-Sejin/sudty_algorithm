4b07d46d6c379bc6442f27f989571b2b5f8731c0
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-04-17 17:37:56 +0900

    [D1] Title: 서랍의 비밀번호, Time: 139 ms, Memory: 26,720 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner in = new Scanner(System.in);
+		int p = in.nextInt();
+		int k = in.nextInt();
+        
+		System.out.println(p-k+1);
+	}
+}
