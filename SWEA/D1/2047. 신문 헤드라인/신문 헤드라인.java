a2765a30fdb4a9db0b6407a20c2bfb4637f0562e
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-04-17 17:32:25 +0900

    [D1] Title: 신문 헤드라인, Time: 117 ms, Memory: 27,204 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner in = new Scanner(System.in);
+		String str=in.next();
+
+		System.out.println(str.toUpperCase());
+	}
+}
