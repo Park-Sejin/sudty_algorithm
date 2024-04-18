54a40da08717392a3aeb17d96c2da233de980b9f
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-04-18 15:28:08 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 138 ms, Memory: 25,488 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+        
+        while(n >= 0){
+            System.out.print(n-- + " ");
+        }
+	}
+}
