c37e6da84c59d3b548d7771df0dcf8b5b700775e
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-04-19 11:14:11 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int m = s.length()/2;
+        
+        if(s.length() % 2 == 0){
+            answer = s.substring(m-1, m+1);
+        }
+        else {
+            answer = s.substring(m, m+1);
+        }
+        
+        return answer;
+    }
+}
