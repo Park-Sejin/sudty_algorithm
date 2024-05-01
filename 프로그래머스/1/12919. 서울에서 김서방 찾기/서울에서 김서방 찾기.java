47862c32c3b779516052997f66a4859328bcf65d
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-05-01 16:47:27 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.90 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,8 +2,13 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] seoul) {
-        int index = Arrays.asList(seoul).indexOf("Kim");
-        String answer = "김서방은 " + index + "에 있다";
+        String answer = "";
+        
+        for(int i = 0; i < seoul.length; i++){
+            if("Kim".equals(seoul[i])){
+                return "김서방은 " + i + "에 있다";
+            }
+        }
         
         return answer;
     }
