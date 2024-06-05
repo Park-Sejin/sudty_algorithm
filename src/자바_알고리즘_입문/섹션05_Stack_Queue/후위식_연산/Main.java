bfd4baba67403d5916cc045f65568e0600bafb3d
Park-Sejin <stsj84@gmail.com>
2024-06-05 23:34:32 +0900

    [2024/06/05] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Stack, Queue(자료구조)  - 후위식 연산(postfix)

---
+++ src/자바_알고리즘_입문/섹션05_Stack_Queue/후위식_연산/Main.java
@@ -0,0 +1,37 @@
+package 자바_알고리즘_입문.섹션05_Stack_Queue.후위식_연산;
+
+import java.util.*;
+
+class Main {
+	public void solution(String str){
+		Stack<Integer> stack = new Stack<>();
+		
+		for(char ch : str.toCharArray()) {
+			if(ch >= '1' && ch <= '9') {
+				stack.push(Integer.parseInt(ch+""));
+			}
+			else {
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				
+				switch(ch) {
+					case '+' : stack.push(num2 + num1); break;
+					case '-' : stack.push(num2 - num1); break;
+					case '*' : stack.push(num2 * num1); break;
+					case '/' : stack.push(num2 / num1); break;
+				}
+			}
+		}
+		
+		System.out.print(stack.peek());
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner in = new Scanner(System.in);
+		
+		String str = in.next();
+		
+		T.solution(str);
+	}
+}
