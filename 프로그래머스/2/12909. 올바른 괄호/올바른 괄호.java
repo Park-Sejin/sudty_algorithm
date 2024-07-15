eb253ce6c1f35c6f93a5c965f77e0be137f19410
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-07-16 01:09:38 +0900

    [level 2] Title: 올바른 괄호, Time: 28.52 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                stack.push('(');
+            }
+            else if(stack.isEmpty() || stack.pop() == ')'){
+                return false;
+            }
+        }
+        
+        return (stack.isEmpty())? true : false;
+    }
+}
