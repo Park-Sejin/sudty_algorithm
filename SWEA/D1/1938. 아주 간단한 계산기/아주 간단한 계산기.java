a3dca01c0e2fd5cba8cdfede299f6445a86b1631
Park-Sejin <42609731+Park-Sejin@users.noreply.github.com>
2024-07-18 23:48:16 +0900

    [D1] Title: 아주 간단한 계산기, Time: 137 ms, Memory: 21,452 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+	}
+}
