f3130296a8606e6ef6389f585584c78668e43b48
Park-Sejin <stsj84@gmail.com>
2024-07-21 22:42:46 +0900

    [2024/07/21] 자바(Java) 알고리즘 문제풀이 입문: 코딩테스트 대비 1. Stack, Queue(자료구조)  - 삽입 정렬

---
+++ src/자바_알고리즘_입문/섹션06_정렬/삽입정렬/Main.java
@@ -0,0 +1,35 @@
+package 자바_알고리즘_입문.섹션06_정렬.삽입정렬;
+
+import java.util.*;
+
+class Main {
+	public void solution(int[] arr){
+		for(int i = 1; i < arr.length; i++) {
+			for(int j = i; j > 0; j--) {
+				if(arr[j] < arr[j-1]) {
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
+			}
+		}
+		
+		for(int num : arr) {
+			System.out.print(num + " ");
+		}
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		T.solution(arr);
+	}
+}
